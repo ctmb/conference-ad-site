925525fa103bd291fde9fe22fbbe2ee1c8ee5f45
Carson McBride <mcbride.carson@gmail.com>
2017-06-18 19:22:24 -0700

    This branch holds an attempt at making the entire site reative, including the landing page

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import {render} from "react-dom";
 import App from "./components/app.jsx";
 import Tour from "./components/tour.jsx";
-import About from "./components/about.jsx";
-import Contact from "./components/contact.jsx";
 
 //import our stylesheet so webpack puts it into the bundle
 import "./css/main.css";
+import "./css/homepage.css";
+import "./css/footer.css";
 
 //replace the JSX here with a Router configuration
 import {Router, Route, IndexRoute, hashHistory} from "react-router";
@@ -15,10 +15,7 @@ var router = (
     <Router history={hashHistory}>
         <Route path="/" component={Tour}>
             <IndexRoute component={App}></IndexRoute>
-            <Route path="/about" component={About}></Route>
-            <Route path="/contact" component={Contact}></Route>
         </Route>
     </Router>
 );
-
-render(router, document.getElementById("app"));
+render(router, document.getElementById("tour"));
