5ba368a9cdc8d1dfee513b79f3dcab2ac0d948cd
Nikolas Tokuno <nikolas.tokuno@wsu.edu>
2017-06-19 20:18:11 -0700

    Added speakers.html page

--- src/components/speakers.jsx
+++ src/components/speakers.jsx
@@ -12,6 +12,38 @@ export default class extends React.Component {
 
     render() {
         return (
+         <Navbar inverse collapseOnSelect fixedTop>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="./index.html">Overview</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle/>
+                </Navbar.Header>
+                    <Navbar.Collapse>
+                        <Nav pullRight>
+                            <NavItem>
+                                <IndexLink to="/" activeClassName="active">
+                                    Speakers
+                                </IndexLink>
+                            </NavItem>
+                            <NavItem>
+                                <Link to="/agenda" activeClassName="active">
+                                    Agenda
+                                </Link>
+                            </NavItem>
+                            <NavItem>
+                                <Link to="/venue" activeClassName="active">
+                                    Venue
+                                </Link>
+                            </NavItem>
+                            <NavItem>
+                                <Link to="/sponsors" activeClassName="active">
+                                    Sponsors
+                                </Link>                                
+                            </NavItem>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
             <div>
                 {this.state}
                 <h1>Speakers</h1>
